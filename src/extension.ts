bef050fdcec60f2234f7688d1d94b46d4b1748ad
Appleblue17 <hluo262@connect.hkust-gz.edu.cn>
2025-05-01 12:13:29 +0800

    [feat] Add support for Notesaw language and preview command in VSCode extension

--- src/extension.ts
+++ src/extension.ts
@@ -8,19 +8,39 @@ import noteProcess from "./note.ts";
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "notesaw" is now active!');
 
+  let panel: vscode.WebviewPanel | undefined = undefined;
+
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   context.subscriptions.push(
-    vscode.commands.registerCommand("notesaw.start", async () => {
-      const panel = vscode.window.createWebviewPanel("notesaw", "Notesaw", vscode.ViewColumn.Two, {
-        localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, "assets")], // Only allow access to this directory
-        enableScripts: true, // Make sure scripts are enabled if needed
-      });
-
+    vscode.commands.registerCommand("notesaw.showPreview", async () => {
       const editor = vscode.window.activeTextEditor;
       if (editor) {
-        console.log("type", editor.document.languageId);
+        if (editor.document.languageId !== "notesaw") {
+          vscode.window.showErrorMessage("Please open a notesaw file.");
+          return;
+        }
+
+        if (!panel) {
+          panel = vscode.window.createWebviewPanel(
+            "notesaw",
+            "Notesaw",
+            { viewColumn: vscode.ViewColumn.Beside, preserveFocus: true },
+            {
+              localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, "assets")], // Only allow access to this directory
+              enableScripts: true, // Make sure scripts are enabled if needed
+            }
+          );
+
+          panel.onDidDispose(
+            () => {
+              panel = undefined; // Reset the panel when it's closed
+            },
+            null,
+            context.subscriptions
+          );
+        }
 
         const noteCssUri = panel.webview.asWebviewUri(
           vscode.Uri.joinPath(context.extensionUri, "assets", "styles", "note.css")
